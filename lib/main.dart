14cdf262ca27b3f77d1a527059ecb646d177360b
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2020-01-25 21:13:01 +0300

    Create the application. Part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart' as english_words;
 
-void main() => runApp(MyApp());
+void main() => runApp(PolygonApp());
 
-class MyApp extends StatelessWidget {
+class PolygonApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-    );
+    return MaterialApp(title: 'Startup name generator', home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <String>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+        ),
+        body: _buildSuggestions());
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(english_words
+                .generateWordPairs()
+                .map((pair) => pair.asPascalCase)
+                .take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(String title) {
+    return ListTile(title: Text(title, style: TextStyle(fontSize: 18.0)));
   }
 }
